2ba4207faa5f1d34b2df10ec25f40fe5c34da5f6
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2024-07-31 22:15:19 +0200

    Base Settings+++++++ Version!

--- app/Modules/Base/Controllers/HomeController.php
+++ app/Modules/Base/Controllers/HomeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Modules\Base\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Modules\Base\Helpers\Version;
 use Inertia\Inertia;
 
 class HomeController extends Controller
@@ -16,7 +17,8 @@ class HomeController extends Controller
     public function index(): \Inertia\Response
     {
         return Inertia::render('Home', [
-
+            'version' => Version::version,
+            'updates' => Version::updates(),
         ]);
     }
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,22 @@
 <template>
     <div>
-        <Head title="Dashboard" />
-        <h1>Добро пожаловать в Систему Управления Услугами</h1>
+        <Head>
+            <title>SeDeSys - CRM управления услугами v{{ version }}</title>
+        </Head>
+        <h1 class="font-medium">Добро пожаловать в Систему Управления Услугами</h1>
+        <div class="mt-4 bg-white rounded-lg p-4">
+            <div v-for="(items, index) in updates">
+                <div class="font-medium mt-3">
+                    {{ index}}
+                </div>
+                <ul class="mt-1">
+                    <li v-for="item in items" class="text-sm ml-3">
+                        <span class="circle green"></span>
+                        {{ item }}
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -12,6 +27,10 @@ import Layout from '@/Components/Layout.vue'
 export default {
     components: {
         Head,
+    },
+    props: {
+        version: String,
+        updates: Array,
     },
     layout: Layout,
 }
