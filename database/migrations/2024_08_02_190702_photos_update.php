5c1a3807b69be4119229d1a495c36dca0352e262
Roman-Moiseenko <r.a.moiseenko@gmail.com>
2024-08-02 21:14:23 +0200

    Base Settings+++++++ Gallery++

--- database/migrations/2024_08_02_190702_photos_update.php
+++ database/migrations/2024_08_02_190702_photos_update.php
@@ -23,8 +23,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('photos', function (Blueprint $table) {
-            $table->dropColumn('title');
-            $table->dropColumn('description');
         });
     }
 };
